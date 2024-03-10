6c8483400685d587e2d353da6132115ffc41bade
Saad Qais <saadqais2013@live.com>
2024-03-10 14:28:11 +0300

    Add project files.

---
+++ AddressBookApplication/Controllers/AddressBooksController.cs
@@ -0,0 +1,152 @@
+﻿using AddressBookApplication.Data;
+using AddressBookApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBookApplication.Controllers
+{
+    public class AddressBooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddressBooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AddressBooks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AddressBooks.ToListAsync());
+        }
+
+        // GET: AddressBooks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addressBook = await _context.AddressBooks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addressBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(addressBook);
+        }
+
+        // GET: AddressBooks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddressBooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Company,Address,Phone,Email")] AddressBook addressBook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(addressBook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addressBook);
+        }
+
+        // GET: AddressBooks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addressBook = await _context.AddressBooks.FindAsync(id);
+            if (addressBook == null)
+            {
+                return NotFound();
+            }
+            return View(addressBook);
+        }
+
+        // POST: AddressBooks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Company,Address,Phone,Email")] AddressBook addressBook)
+        {
+            if (id != addressBook.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(addressBook);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressBookExists(addressBook.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addressBook);
+        }
+
+        // GET: AddressBooks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addressBook = await _context.AddressBooks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addressBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(addressBook);
+        }
+
+        // POST: AddressBooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var addressBook = await _context.AddressBooks.FindAsync(id);
+            if (addressBook != null)
+            {
+                _context.AddressBooks.Remove(addressBook);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressBookExists(int id)
+        {
+            return _context.AddressBooks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AddressBookApplication/Models/AddressBook.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookApplication.Models
+{
+    public class AddressBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Company { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+    }
+}
